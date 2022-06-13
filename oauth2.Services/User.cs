7e3a71f30ef94229e48393cc4d327169ea82783b
jeeeanlefchak <jean_2103@hotmail.com>
2022-06-13 14:55:12 -0300

    JWT

---
+++ oauth2.Services/User.cs
@@ -0,0 +1,10 @@
+﻿namespace oauth2.Services
+{
+    public class User
+    {
+        public string UserName { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+    }
+}
